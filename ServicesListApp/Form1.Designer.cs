4c3552cc9285e516ae5cd013bc549abf4ec94265
mwitkowski <mwitkowski@DESKTOP-U2TEESV>
2021-09-02 13:54:08 +0200

    update

--- ServicesListApp/Form1.Designer.cs
+++ ServicesListApp/Form1.Designer.cs
@@ -29,11 +29,19 @@ namespace ServicesListApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lbServices = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbServices
             // 
+            this.lbServices.ContextMenuStrip = this.contextMenuStrip1;
             this.lbServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.lbServices.FormattingEnabled = true;
             this.lbServices.ItemHeight = 29;
@@ -42,15 +50,56 @@ namespace ServicesListApp
             this.lbServices.Size = new System.Drawing.Size(1050, 323);
             this.lbServices.TabIndex = 0;
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.stopToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(110, 58);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(109, 24);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(106, 6);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(109, 24);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(850, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1074, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lbServices);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -58,6 +107,11 @@ namespace ServicesListApp
         #endregion
 
         private System.Windows.Forms.ListBox lbServices;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ServicesListApp/Form1.cs
+++ ServicesListApp/Form1.cs
@@ -21,7 +21,7 @@ namespace ServicesListApp
         private void GetAllServices()
         {
             lbServices.Items.Clear();
-            foreach(var service in ServiceController.GetServices())
+            foreach(var service in ServiceController.GetServices(".")) // . means Local machine
             {
                 string serviceName = service.ServiceName;
                 string serviceDisplayName = service.DisplayName;
@@ -33,6 +33,55 @@ namespace ServicesListApp
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllServices();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ManageService(1);
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ManageService(0);
+        }
+
+        private void ManageService(int operation)
+        {
+            String serviceName = lbServices.SelectedItem.ToString().Split('|')[0].Trim();
+            ServiceController service = new ServiceController(serviceName);
+            if (operation == 1)
+            {
+                service.Start();
+                //service.WaitForStatus(ServiceControllerStatus.Running);
+
+                try
+                {
+                    service.WaitForStatus(ServiceControllerStatus.Stopped, new TimeSpan(0, 0, 10));
+                    MessageBox.Show("Running");
+                } catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+            else if (operation == 0)
+            {
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped);
+                MessageBox.Show("Stopped");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect operation");
+            }
+
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
             GetAllServices();
         }
