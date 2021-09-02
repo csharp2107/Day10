4cde9f4f2c9574adf88f125fdd212bb492acdc75
mwitkowski <mwitkowski@DESKTOP-U2TEESV>
2021-09-02 12:01:09 +0200

    Add project files.

---
+++ ServicesListApp/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ServicesListApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbServices = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbServices
+            // 
+            this.lbServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbServices.FormattingEnabled = true;
+            this.lbServices.ItemHeight = 29;
+            this.lbServices.Location = new System.Drawing.Point(12, 12);
+            this.lbServices.Name = "lbServices";
+            this.lbServices.Size = new System.Drawing.Size(1050, 323);
+            this.lbServices.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 450);
+            this.Controls.Add(this.lbServices);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbServices;
+    }
+}
+
---
+++ ServicesListApp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServicesListApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetAllServices()
+        {
+            lbServices.Items.Clear();
+            foreach(var service in ServiceController.GetServices())
+            {
+                string serviceName = service.ServiceName;
+                string serviceDisplayName = service.DisplayName;
+                string serviceType = service.ServiceType.ToString();
+                string status = service.Status.ToString();
+
+                lbServices.Items.Add($"{serviceName} | {serviceDisplayName} | {serviceType} | {status}");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllServices();
+        }
+    }
+}
